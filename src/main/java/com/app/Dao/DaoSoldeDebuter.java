95df2209f63ebef7e3450b50b8f8104ba75f9c0e
rawade <rawadegroupe@gmail.com>
2021-02-18 01:09:25 +0000

    first commit

---
+++ src/main/java/com/app/Dao/DaoSoldeDebuter.java
@@ -0,0 +1,14 @@
+package com.app.Dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.metier.entities.SoldeDebuterJournee;
+@Repository
+public interface DaoSoldeDebuter extends JpaRepository<SoldeDebuterJournee,Integer> {
+	SoldeDebuterJournee findByIdU(int id);
+	SoldeDebuterJournee findById(int userId);
+	List<SoldeDebuterJournee> findByIdUAndStatus(int userId,int idU);
+}
